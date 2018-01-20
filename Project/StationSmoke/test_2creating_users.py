75a73bebbe32b0d06b469b83d1600126b72d033d
Lunchesque <Lunchesque1993@gmail.com>
2018-01-20 11:33:58 +0300

    a bit of tests good look

--- Project/StationSmoke/test_2creating_users.py
+++ Project/StationSmoke/test_2creating_users.py
@@ -7,4 +7,21 @@ from model.data import Data
 def test_creating_users(app):
     credsOper = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
                                                                                         phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
     app.users.creating_operator(credsOper)
+    assert len(old_users) + 1 == len(app.users.get_users_list())
+    new_users = app.users.get_users_list()
+
+    credsWatcher = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
+                                                                                            phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
+    app.users.creating_watcher(credsWatcher)
+    assert len(old_users) + 1 == len(app.users.get_users_list())
+    new_users = app.users.get_users_list()
+
+    credsDemo = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
+                                                                                        phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
+    app.users.creating_demo(credsDemo)
+    assert len(old_users) + 1 == len(app.users.get_users_list())
+    new_users = app.users.get_users_list()
--- Project/tests/test_add_all_types_of_users.py
+++ Project/tests/test_add_all_types_of_users.py
@@ -5,34 +5,31 @@ from string import digits
 from model.data import Data
 
 
-def test_creating_admin(app):
-    old_users = app.users.get_users_list()
+def test_creating_all_types_of_users(app):
     credsAdm = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
                                                                                         phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
     app.users.creating_admin(credsAdm)
     assert len(old_users) + 1 == len(app.users.get_users_list())
     new_users = app.users.get_users_list()
 
-def test_creting_operator(app):
-    old_users = app.users.get_users_list()
     credsOper = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
                                                                                         phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
     app.users.creating_operator(credsOper)
     assert len(old_users) + 1 == len(app.users.get_users_list())
     new_users = app.users.get_users_list()
 
-def test_creating_watcher(app):
-    old_users = app.users.get_users_list()
     credsWatcher = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
                                                                                             phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
     app.users.creating_watcher(credsWatcher)
     assert len(old_users) + 1 == len(app.users.get_users_list())
     new_users = app.users.get_users_list()
 
-def test_creating_demo(app):
-    old_users = app.users.get_users_list()
     credsDemo = Data(email = "AutoTestUser_{}_{}@ki.ki", name = "Auto.test.user_{}_{}", userId = (''.join(choice(digits) for i in range(5))),
                                                                                         phone = (''.join(choice(digits) for i in range(15))))
+    old_users = app.users.get_users_list()
     app.users.creating_demo(credsDemo)
     assert len(old_users) + 1 == len(app.users.get_users_list())
     new_users = app.users.get_users_list()

eed4bac05a30d52a52673c940637474b72a9429d
lunchesque <lunchesque1993@gmail.com>
2018-02-05 17:13:07 +0300

    2

--- Project/fixture/clouds.py
+++ Project/fixture/clouds.py
@@ -17,10 +17,11 @@ class CloudsHelper:
         driver.find_element_by_xpath("//button[@id='copy_vpnlink']").click()
         vpnlink = os.popen('xsel').read()
         self.app.session.login_server()
+        ActionChains(driver).pause(3).perform()
         driver.find_element_by_xpath("//div[@kp-icon-svg='settings-20']").click()
         driver.find_element_by_link_text("Сеть").click()
         driver.find_element_by_link_text("Облака").click()
-        if len(driver.find_elements_by_xpath("//tr")) > 0:
+        if len(driver.find_elements_by_xpath("//tr")) > 1:
             driver.find_element_by_xpath("//button[@name='del0']").click()
             driver.find_element_by_xpath("//button[@ng-click='confirm()']").click()
         ActionChains(driver).pause(0.3).perform()

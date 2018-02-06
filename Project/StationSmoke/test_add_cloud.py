2d942c3a4a8f7c56b6628c0b4615debfc7490a64
lunchesque <lunchesque1993@gmail.com>
2018-02-06 15:40:36 +0300

    1

--- Project/StationSmoke/test_add_cloud.py
+++ Project/StationSmoke/test_add_cloud.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.mark.run(order = 5)
+@pytest.mark.run(order = 6)
 def test_add_cloud(app):
     app.clouds.add_cloud()
     assert app.clouds.get_servs_list() > 0
--- Project/StationSmoke/test_delete_place.py
+++ Project/StationSmoke/test_delete_place.py
@@ -4,6 +4,6 @@ import pytest
 
 
 #@pytest.mark.skip(reason="no way of currently testing this")
-#@pytest.mark.run(order = 2)
+@pytest.mark.run(order = 5)
 def test_deleting_auto_places(app):
     app.places.deletion_auto_places()
